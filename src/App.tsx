442b71b5723abdcf78015428acb1dbb902c8250d
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-02-12 16:57:34 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import TodoList, {TasksType, TodoListType} from "./TodoList";
 import {v1} from "uuid";
 
+
 function App() {
 
     let [tasks, setTasks] = useState<TasksType[]>([
@@ -11,13 +12,26 @@ function App() {
         {id: v1(), title: 'WFT', isDone: false}
     ])
 
+const RemoveTasks = (id:string) => {
+    let deleteTask = tasks.filter(t=>t.id !== id)
+    setTasks(deleteTask)
+}
 
+const AddTasks = (title:string) =>{
+        let task = {id: v1(), title: title, isDone: true}
+    let NewTasks = [ task,...tasks]
+    setTasks(NewTasks)
+    }
+
+    let FilteredTask
 
     return (
         <div className="App">
             <TodoList
                 title={'WTF'}
                 tasks={tasks}
+                RemoveTasks={RemoveTasks}
+                AddTasks={AddTasks}
             />
         </div>
     );
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {v1} from "uuid";
 
 export type TasksType = {
     id: string
@@ -9,6 +10,8 @@ export type TasksType = {
 export type TodoListType = {
     tasks: TasksType[]
     title: string
+    RemoveTasks: (id: string) => void
+    AddTasks: (title:string) => void
 }
 
 
@@ -18,13 +21,13 @@ function TodoList(props: TodoListType) {
             <h3>{props.title}</h3>
             <div>
                 <input/>
-                <button>+</button>
+                <button onClick={()=>{props.AddTasks('new')}}>+</button>
             </div>
             <ul>
                 {props.tasks.map((t) => {
                     return (
-                        <li><input type="checkbox" checked={t.isDone}/> <span>{t.title}</span>
-                        <button>x</button>
+                        <li key={v1()}><input type="checkbox" checked={t.isDone}/> <span>{t.title}</span>
+                        <button onClick={()=>{props.RemoveTasks(t.id)}}>x</button>
                         </li>
                     )
                 })
